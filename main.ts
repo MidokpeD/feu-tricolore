11c162d3408701cd856a733414f60273ea5c9260
MidokpeD <160834660+MidokpeD@users.noreply.github.com>
2026-01-17 17:22:17 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.A)) {
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(100)
+        basic.showString("STOP")
+    } else {
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(100)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(100)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
+        basic.pause(100)
+    }
+})
